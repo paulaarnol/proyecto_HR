c92541fd48d0998e88e3dc827ea8a9f62f448f9f
paulaarnol <paulalihuearnolacuna@gmail.com>
2025-10-08 11:16:19 -0300

    Agregando else

--- JS/productos.js
+++ JS/productos.js
@@ -2,11 +2,14 @@ const cards = document.querySelectorAll('.taller');
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
-        if (card.classList.contains('active')) {
+        if (card.classList.contains('active')){
             cards.forEach(c => {
                 c.classList.remove('active');
                 c.classList.remove('dimmed');
           });
+        }
+        else{
+
         }
     });
 });
