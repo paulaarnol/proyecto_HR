a274b8b69bea80f28506b141cc7f5882a8cb4bc1
paulaarnol <paulalihuearnolacuna@gmail.com>
2025-10-08 11:12:30 -0300

    Agregando condición para verificar si card tieene clase active

--- JS/productos.js
+++ JS/productos.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.taller');
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
-        
+        if (card.classList.contains('active')) {
+            
+        }
     });
 });
