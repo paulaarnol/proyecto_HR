e01619cc0f98423a90c99a401af24117359365cc
Candela-vlqz <candelavlqzepet20@gmail.com>
2025-10-22 02:31:06 -0300

    Añadiendo posicion aleatoria de particulas

--- JS/contacto.js
+++ JS/contacto.js
@@ -12,4 +12,6 @@ function crearParticulas(contenedor, cantidad) {
     for (let i = 0; i < cantidad; i++) {
         const part = document.createElement("div");
         part.classList.add("particula");
+        part.style.left = Math.random() * 100 + "%";
+    part.style.top = Math.random() * 100 + "%";
     }
