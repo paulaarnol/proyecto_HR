81133b687424a5e273567955fe1ddf3c6974bc36
Candela-vlqz <candelavlqzepet20@gmail.com>
2025-10-26 04:53:32 -0300

    Modificando color de clase contacto

--- JS/contactos.js
+++ JS/contactos.js
@@ -14,15 +14,16 @@ botones.forEach(boton => {
 });
 
 /*Mi parte*/
-function crearParticula(contenedor, cantidad) {
-    for (let i = 0; i < cantidad; i++) {
-        const part = document.createElement("div");
-        part.classList.add("particula");
-        part.style.left = Math.random() * 100 + "%";
-        part.style.top = Math.random() * 100 + "%";
-        part.style.animationDuration = 5 + Math.random() * 10 + "s";
+function crearParticulas(contenedor, cantidad) {
+  for (let i = 0; i < cantidad; i++) {
+    const part = document.createElement("div");
+    part.classList.add("particula");
+    part.style.left = Math.random() * 100 + "%";
+    part.style.top = Math.random() * 100 + "%";
+    part.style.animationDuration = 5 + Math.random() * 10 + "s";
     part.style.animationDelay = Math.random() * 5 + "s";
     contenedor.appendChild(part);
-    }
+  }
 }
-crearParticula(document.body, 40);
+
+crearParticulas(document.body, 100);
