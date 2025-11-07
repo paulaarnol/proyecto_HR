9af2f1818be94c20222ee47927978cb8a3b1a7c2
cande012 <tanetane117@gmail.com>
2025-11-07 09:57:50 -0300

    Agrego menutoggle.addEventListener.

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,11 @@ console.log ("script productos,js cargado");
 const menutoggle= document.getElementById("menu-toggle");
 const navegacion - document.getElementById("navegacion");
 
-if (menutoggle && navegacion)
+if (menutoggle && navegacion){
+menutoggle.addEventListener
+
+
+}
 
 
 
