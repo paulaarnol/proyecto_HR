e41e5d16b52fa1ebd8a99f6e3b78abb778c5d208
cande012 <tanetane117@gmail.com>
2025-11-07 14:41:43 -0300

    Arreglando js

--- JS/index.js
+++ JS/index.js
@@ -1,39 +1,46 @@
-modal js 
-document.addEventListener("DOMContentLoaded",function() {
-console.log ("script productos,js cargado");
-const menutoggle= document.getElementById("menu-toggle");
+ const hora = new Date().getHours();
+     const mensaje = document.createElement("p");
+     if (hora >= 9 && hora < 20) {
+          mensaje.textContent = "🟢 Estamos abiertos de 9:00 a 20:00.";
+}else {
+  mensaje.textContent = "🔴 Cerrado. Nuestro horario es de 9:00 a 20:00.";
+}
+document.querySelector("footer").appendChild(mensaje);
+
+MODAL *js*
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script productos.js cargado");
+const menuToggle = document.getElementById("menu-toggle");
 const navegacion = document.getElementById("navegacion");
 
-if (menutoggle && navegacion){
-menutoggle.addEventListener("click", () => {
-navegacion.classList.toggle("active");
-
-
+if (menuToggle && navegacion) {
+menuToggle.addEventListener("click", () => {
+  navegacion.classList.toggle("active");
 });
 }
-const modal= document.getElementById("modal-cargando"); 
-const navbuttons = document.querySelectorAll('#navegacion a');
-if(modal && navbuttons.length > 0) {
-navbuttons.forEach(boton => {
-boton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const destino = boton.getAttribute ('href');
-mostrarmodal(destino)
+const modal = document.getElementById("modal-cargando");
+const navButtons = document.querySelectorAll('#navegacion a');
+if (modal && navButtons.length > 0) {
+navButtons.forEach(boton => {
+  boton.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    const destino = boton.getAttribute('href');
+    mostrarModal(destino);
+  });
+  boton.addEventListener('contextmenu', (e) => {
+    e.preventDefault(); 
+    const destino = boton.getAttribute('href');
+    mostrarModal(destino);
+      });
+    });
+  }
 
-});
-Boton.addEventListener('contextmenu', (e) => {
-e.preventDefault();
-const destino=boton.getAttribute('href');
-mostrarmodal(destino);
- });
-});
-}
-  function mostrarmodal(destino){
-modal.style.display='flex';
-console.log("mostrando modal...");
-setTimeout(() => { 
-console.log("redirigiendo a:", destino);
-window.location.href =destino;
- }, 1500);
+function mostrarModal(destino) {
+  modal.style.display = 'flex';
+  console.log("Mostrando modal...");
+  setTimeout(() => {
+    console.log("Redirigiendo a:", destino);
+    window.location.href = destino;
+  }, 1500);
 }
 });
