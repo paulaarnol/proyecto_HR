d66972960c53113ab93e39fc2ea3b7aeac65ca5c
cande012 <tanetane117@gmail.com>
2025-11-07 10:20:46 -0300

    Agrego navbuttons y modifico

--- JS/index.js
+++ JS/index.js
@@ -14,13 +14,21 @@ navegacion.classlist.toggle("active");
 const modal= document.getElementById("modal-cargando"); 
 const navbuttons - document.querySelectorAll('#navegacion a');
 if(modal && navbuttons.length > 0) {
+navbuttons.forEach(boton => {
+
 
 
 
     
+}
+    
+});
+
+
+
 } 
  
-
+)
 
 
 
