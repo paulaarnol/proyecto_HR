224443936b9e55839e7e7fd985ea49f435de53aa
Candela-vlqz <candelavlqzepet20@gmail.com>
2025-11-07 17:00:53 -0300

    Añadiendo carga/contacto.js

--- JS/contactos.js
+++ JS/contactos.js
@@ -79,3 +79,67 @@ function crearParticulas(contenedor, cantidad) {
 }
 
 crearParticulas(document.body, 100);
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script productos.js cargado");
+
+  const menuToggle = document.getElementById("menu-toggle");
+  const navegacion = document.getElementById("navegacion");
+
+  if (!menuToggle || !navegacion) {
+    console.error("No se encontraron los elementos del menú");
+    return;
+  }
+
+  function toggleMenu() {
+    navegacion.classList.toggle("active");
+  }
+
+  menuToggle.addEventListener("click", toggleMenu);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script productos.js cargado");
+const menuToggle = document.getElementById("menu-toggle");
+const navegacion = document.getElementById("navegacion");
+
+if (menuToggle && navegacion) {
+menuToggle.addEventListener("click", () => {
+  navegacion.classList.toggle("active");
+});
+}
+const modal = document.getElementById("modal-cargando");
+const navButtons = document.querySelectorAll('#navegacion a');
+if (modal && navButtons.length > 0) {
+navButtons.forEach(boton => {
+  boton.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    const destino = boton.getAttribute('href');
+    mostrarModal(destino);
+  });
+  boton.addEventListener('contextmenu', (e) => {
+    e.preventDefault(); 
+    const destino = boton.getAttribute('href');
+    mostrarModal(destino);
+      });
+    });
+  }
+
+function mostrarModal(destino) {
+  modal.style.display = 'flex';
+  console.log("Mostrando modal...");
+  setTimeout(() => {
+    console.log("Redirigiendo a:", destino);
+    window.location.href = destino;
+  }, 1500);
+}
+});
+
+ const hora = new Date().getHours();
+     const mensaje = document.createElement("p");
+     if (hora >= 9 && hora < 20) {
+          mensaje.textContent = "🟢 Estamos abiertos de 9:00 a 20:00.";
+}else {
+  mensaje.textContent = "🔴 Cerrado. Nuestro horario es de 9:00 a 20:00.";
+}
+document.querySelector("footer").appendChild(mensaje);
