be2477a068364ddcca642a34fd07d01eceb05757
cande012 <tanetane117@gmail.com>
2025-11-07 10:51:54 -0300

    Agrego function y modifico.

--- JS/index.js
+++ JS/index.js
@@ -14,7 +14,7 @@ navegacion.classlist.toggle("active");
 const modal= document.getElementById("modal-cargando"); 
 const navbuttons - document.querySelectorAll('#navegacion a');
 if(modal && navbuttons.length > 0) {
-navbuttons.forEach(boton  {
+navbuttons.forEach(boton,  {
 boton.addEventListener('click', (e) => {
     e.preventDefault();
     const destino = boton.getAttribute ('href');
@@ -25,7 +25,14 @@ Boton.addEventListener('contextmenu', (e) {
 e.preventDefault();
 const destino=boton.getAttribute('href');
 mostrarmodal(destino);
-  
+ }),
+});
+}
+  function mostrarmodal(desstino){
+
+
+    
+  }
 
 
 
